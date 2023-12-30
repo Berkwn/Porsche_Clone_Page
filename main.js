63cc354cc75c92e202de8130b2c40e1aee9b95e6
Berkwn <berkand180@gmail.com>
2023-12-30 10:20:50 +0300

    Porsche_Clone_First_Page
    I'm cloned Porsche page.

---
+++ main.js
@@ -0,0 +1,22 @@
+function openNav(){
+
+
+    document.getElementById('myMainMenu').style.width="800px"
+    document.getElementById('myMainMenu').style.zIndex="2"
+    document.getElementById('myMainMenu').style.transition="0.5s"
+    document.getElementById('myMainMenu').style.boxShadow="offset-x"
+   
+   
+   
+
+
+}
+
+function closeNav(){
+
+
+    document.getElementById('myMainMenu').style.width="0"
+    document.getElementById('myMainMenu').style.zIndex="0"
+    document.getElementById('myMainMenu').style.transition="0.5s"
+    
+}
